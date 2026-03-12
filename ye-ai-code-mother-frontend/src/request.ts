c320e31a56b6a26b6001be351ce9218cab80ba7d
yyffyyq <35894333@qq.com>
2026-03-12 17:13:31 +0800

    前端-轮播图管理部分： 1、增加了拖拽插入的功能  1.1 先排序，再将id拿出来按这个排序，然后将这个id发送给后台，后台拿到id将sortorder拿出来按照这个id的排序从小到大排序之后重新赋值给对应的id不过这个过程中id的这个数组的序列是不变的  1.2 我认为的难点在于这个拖拽的实现，然后就是这个怎么把卡片的id拿出来发送给后台，发送比较简单，我认为难点或者说是我没理解的点子在于怎么拿出来  1.3 还有一些修改的地方就是常规的生成，对应后台服务的api接口

--- ye-ai-code-mother-frontend/src/request.ts
+++ ye-ai-code-mother-frontend/src/request.ts
@@ -3,7 +3,7 @@ import { message } from 'ant-design-vue'
 
 // 创建 Axios 实例
 const myAxios = axios.create({
-  baseURL: 'http://localhost:8989/api',
+  baseURL: 'http://192.168.2.38:8989/api',
   timeout: 60000,
   withCredentials: true,
 })
